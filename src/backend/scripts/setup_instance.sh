663e90ebe529deef11ec7ad782aa55668a78eda8
RohanSogani <ronsogani@gmail.com>
2020-12-08 12:55:12 -0800

    Update Setup script

--- src/backend/scripts/setup_instance.sh
+++ src/backend/scripts/setup_instance.sh
@@ -6,6 +6,6 @@ sudo apt-get -y install python3-pip
 
 pip3 install --upgrade pip
 
-pip3 install -r /home/ecs289gnlp/textS/src/pegasus/requirements.txt
+pip3 install -r ~/textS/src/pegasus/requirements.txt
 
-pip3 install -r /home/ecs289gnlp/textS/requirements.txt
+pip3 install -r ~/textS/requirements.txt
