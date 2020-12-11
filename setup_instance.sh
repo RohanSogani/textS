f57c8bad3767895c44e3786ecd63fa06377f4d9c
RohanSogani <ronsogani@gmail.com>
2020-12-11 13:10:40 -0800

    Add Java requirement to setup_instance.s

--- setup_instance.sh
+++ setup_instance.sh
@@ -15,3 +15,7 @@ pip3 install -r ~/textS/requirements.txt
 curl -sL https://deb.nodesource.com/setup_15.x | sudo -E bash -
 
 sudo apt-get install -y nodejs
+
+sudo apt install default-jre
+
+sudo apt install default-jdk
