f6b33096a52265eb5ae0d7d1d20e1c70425ff0c1
Holani <priyaholani@gmail.com>
2020-12-10 19:19:02 -0800

    Changing inputtoTFRecord Script

--- src/backend/scripts/inputToTFRecord.py
+++ src/backend/scripts/inputToTFRecord.py
@@ -8,46 +8,7 @@ from pdfminer.layout import LAParams
 from pdfminer.pdfpage import PDFPage
 from nltk import sent_tokenize
 from io import StringIO
-
-def convert_pdf_to_txt(path):
-    """
-    Converts the input pdf file to a plain text
-    """
-    rsrc_mgr = PDFResourceManager()
-    ret_str = StringIO()
-    laparams = LAParams()
-    device = TextConverter(rsrc_mgr, ret_str, laparams=laparams)
-    fp = open(path, 'rb')
-    interpreter = PDFPageInterpreter(rsrc_mgr, device)
-    password = ""
-    maxpages = 0
-    caching = True
-    pagenos = set()
-
-    for page in PDFPage.get_pages(fp, pagenos, maxpages=maxpages, password=password, caching=caching, check_extractable=True):
-        interpreter.process_page(page)
-
-    text = ret_str.getvalue()
-    fp.close()
-    device.close()
-    ret_str.close()
-    return text
-
-def create_sentences(text):
-    """
-    Converts the input text into sentences, also removes stop words
-    """
-    from nltk.corpus import stopwords
-    nltk.download('stopwords')
-    from nltk.tokenize import word_tokenize
-    sentences = sent_tokenize(text)
-    # print("There are ", len(sentences), "sentences in this paper")
-    for i in range(0, 1):
-        text_tokens = word_tokenize(sentences[50])
-        tokens_without_sw = [word for word in text_tokens if not word in stopwords.words()]
-        sentences[i] = (" ").join(tokens_without_sw)
-    print(sentences)
-    return sentences
+import backend.scripts.convertPDF as convert
 
 def main():
     if len(sys.argv) != 2:
@@ -55,15 +16,10 @@ def main():
         exit(0)
 
     pdf_path = str(sys.argv[-1])
-    # print(pdf_path)
-    nltk.download('punkt')
-    text = convert_pdf_to_txt(pdf_path)
-    # Remove references and further
-    text = text.split("References")[0]
-    sentences = create_sentences(text)
+   
+    text = convert.convert_pdf_to_txt(pdf_path)
 
-    sentences = str(sentences)
-    sentences = sentences.replace("\'", "\"")
+    sentences = convert.create_sentences(text)  
 
     input_dict = dict(
         inputs=[sentences],
